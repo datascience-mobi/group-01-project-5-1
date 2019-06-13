5ef23ac5022c514d762b1e2fc6c0c9bb1935e735
WiebkeAlb <49953931+WiebkeAlb@users.noreply.github.com>
2019-06-13 13:00:25 +0200

    better overview QC

--- Code.R
+++ Code.R
@@ -1,10 +1,10 @@
 # Daten auslesen und Matrizen erstellen, mit denen gearbeitet wird
 `ALLBcell` <- readRDS("ALL-Bcell_list.RDS.gz")
 ALLpromotor <- ALLBcell[["promoters"]] 
-ALLpromotor <- ALLpromotor[,c(-4,-5,-6,-8,-9,-10)] # Spalten "Strand", "symbol", "entrezID", "GC", "C", "G" gelöscht
+ALLpromotor <- ALLpromotor[,c(-4,-5,-6,-8,-9,-10)] # Spalten "Strand", "symbol", "entrezID", "GC", "C", "G" gelÃ¶scht
 ALLpromotorCov <- ALLpromotor[,c(15:24)]  # Nur Coverage Daten
 ALLCovMeans <- rowMeans(ALLpromotorCov)   # Liste mit Mittelwert der Coverage pro Genabschnitt
-ALLCovMeansLog <- log(ALLCovMeans)        # Bildung Logarithmus für Plot
+ALLCovMeansLog <- log(ALLCovMeans)        # Bildung Logarithmus fÃ¼r Plot
 
 # Plotten + Speichern der Daten mit Histogramm
 png(filename = "E:/Studium HD/4. FS/Bioinfo05/Hist_noLOG.png", width = 1000, height = 500)
@@ -16,29 +16,17 @@ hist(ALLCovMeansLog, xlab = , ylab = , xlim = c(0,13), ylim = c(0,250), breaks =
 dev.off()
 
 # Threshold bestimmen
-quantile(ALLCovMeans, probs = c(0.05, 0.95),na.rm = TRUE) # Gibt für 5% 139, für 95% 5957 -> 5% zu hohe Grenze 
+quantile(ALLCovMeans, probs = c(0.05, 0.95),na.rm = TRUE) # Gibt fÃ¼r 5% 139, fÃ¼r 95% 5957 -> 5% zu hohe Grenze 
 ALLpromotorCov1 <- ALLpromotorCov 
-ALLpromotorCov1[ALLpromotorCov >= 8342] <- NA # Alle Werte über 99% = NA
-ALLpromotorCov1[ALLpromotorCov1 <= 28] <- NA  # Alle Werte unter 1,5% = NA
+ALLpromotorCov1[ALLpromotorCov > 7753] <- NA # Alle Werte Ã¼ber 98,5% = NA
+ALLpromotorCov1[ALLpromotorCov < 28] <- NA  # Alle Werte unter 1,5% = NA
 
-# Übertragen der Coverage NAs auf die Matrix der Beta-values
-ALLpromotorBeta <- ALLpromotor[,c(5:14)]  # Erstellen Matrix mit nur Beta values 
-for(i in 1:nrow(ALLpromotorCov1)){                      # Abtasten der Zeilen
-     for(j in 1:ncol(ALLpromotorCov1)){                    # Abtasten der Spalten
-         if(is.na(ALLpromotorCov1[i,j])){    # Prüfen ob NA
-             ALLpromotorBeta[i,j] <- NA # Wenn ja, setze Wert bei Beta Values auf NA
-         }
-     } 
-}
-
-# Selbst bei Grenze von 99% 3 Gene mit komplett NA (p73, APAF1, LATS1) 
-
-# Isolieren der relevanten Gene (CDH1, CDKN2A, CDKN2B, CDKN1C, KLK10, DKK3, CDH13, PYCARD, DAPK1, PRKN, PTEN)
+# Isolieren der relevanten Gene (CDH1, CDKN2A, CDKN2B, CDKN1C, KLK10, DKK3, CDH13, PYCARD, DAPK1, PRKN, PTEN, p73, APAF1, LATS1)
 ALLCovGen <- ALLpromotorCov[c("ENSG00000039068","ENSG00000078900","ENSG00000147889","ENSG00000147883","ENSG00000129757","ENSG00000129451","ENSG00000050165","ENSG00000140945","ENSG00000103490","ENSG00000120868","ENSG00000196730","ENSG00000185345","ENSG00000131023","ENSG00000171862"),]
 ALLCov1Gen <- ALLpromotorCov1[c("ENSG00000039068","ENSG00000078900","ENSG00000147889","ENSG00000147883","ENSG00000129757","ENSG00000129451","ENSG00000050165","ENSG00000140945","ENSG00000103490","ENSG00000120868","ENSG00000196730","ENSG00000185345","ENSG00000131023","ENSG00000171862"),] 
 ALLBetaGen <- ALLpromotorBeta[c("ENSG00000039068","ENSG00000078900","ENSG00000147889","ENSG00000147883","ENSG00000129757","ENSG00000129451","ENSG00000050165","ENSG00000140945","ENSG00000103490","ENSG00000120868","ENSG00000196730","ENSG00000185345","ENSG00000131023","ENSG00000171862"),] 
 
-# Überprüfen wie groß der verworfene Anteil
+# ÃœberprÃ¼fen wie groÃŸ der verworfene Anteil
 sum(!is.na(ALLpromotorCov)) # Summe aller Variablen in der Matrix die nicht NAs sind
 'rejectTotal' <- (1-(sum(!is.na(ALLpromotorCov1))/sum(!is.na(ALLpromotorCov)))) # Prozentsatz der verworfenen Daten insgesammt
 'reject' <- (1-(sum(!is.na(ALLCov1Gen))/sum(!is.na(ALLCovGen)))) # Prozentsatz der verworfenen Daten relevanter Gene
@@ -46,33 +34,67 @@ rejectTotal # Ausgabe
 reject # Ausgabe
 View(ALLBetaGen) # Direkt schauen wie viele Gene/Werte NA
 
-# Grenze letztendlich bei 98,5% (7753) gesetzt, da 10% der Daten verworfen + 2 weitere Gene verworfen (p57, PTEN) -> bester Kompromiss
+# Bei Grenze von 99% bereits 3 Gene mit komplett NA (p73, APAF1, LATS1)
+# Grenze letztendlich bei 98,5% (7753) gesetzt: 18% der relevanten Gene verworfen -> 5 Gene (p73, APAF1, LATS1, p57, PTEN) -> bester Kompromiss
+# Insgesamt 6,3% aller Daten verworfen (nur Ã¼ber Threshold)
+
+# Ãœbertragen der Coverage NAs auf die Matrix der Beta-values
+ALLpromotorBeta <- ALLpromotor[,c(5:14)]  # Erstellen Matrix mit nur Beta values 
+for(i in 1:nrow(ALLpromotorCov1)){                      # Abtasten der Zeilen
+  for(j in 1:ncol(ALLpromotorCov1)){                    # Abtasten der Spalten
+    if(is.na(ALLpromotorCov1[i,j])){    # PrÃ¼fen ob NA
+      ALLpromotorBeta[i,j] <- NA # Wenn ja, setze Wert bei Beta Values auf NA
+    }
+  } 
+}
 
 # Eliminieren Gene mit >=4 NAs bei den disease Patienten  
-View(ALLBetaGen)
-for (i in 1:nrow(ALLBetaGen)) {       # Nur relevante Gene
-     if(sum(is.na(ALLBetaGen[i,c(1:5)]))>=4){
-         ALLBetaGen <- ALLBetaGen[-i,]
-     }
-}
-View(ALLBetaGen) 
-
-for (i in 1:nrow(ALLpromotorBeta)) {       # Alle Gene dim(start) = 59808
-  if(sum(is.na(ALLpromotorBeta[i,c(1:5)]))>=4){
-    ALLpromotorBeta <- ALLpromotorBeta[-i,]
+for (j in 1:ncol(ALLpromotorBeta)) {
+  for (i in 1:nrow(ALLpromotorBeta)) {       # Alle Gene dim(start) = 59812
+    if(sum(is.na(ALLpromotorBeta[i,c(1:5)]))>=4){
+      ALLpromotorBeta <- ALLpromotorBeta[-i,]
+    }
   }
 }
-dim(ALLpromotorBeta) # dim(end) = 56138 (6,14% weg)
+dim(ALLpromotorBeta) # dim(end) = 55111 (7,9% weg)
 
-# NAs durch Mean der Zeile ersetzen
-for (j in 1:nrow(ALLBetaGen)) {       # Nur relevante Gene
-     for (i in 1:ncol(ALLBetaGen)) {
-         if(is.na(ALLBetaGen[i,j])){
-             rowMeans(ALLBetaGen[i,], na.rm = TRUE) -> ALLBetaGen[i,j]
+# for (j in 1:ncol(ALLBetaGen)) {  
+#   for (i in 1:nrow(ALLBetaGen)) {       # Nur relevante Gene (Start Anzahl: 14)
+#     if(sum(is.na(ALLBetaGen[i,c(1:5)]))>=4){
+#         ALLBetaGen <- ALLBetaGen[-i,]
+#     }
+#   }
+# }
+# View(ALLBetaGen) # End Anzahl: 9
+
+# Eliminieren Gene mit >=4 NAs bei den healthy Patienten
+for (j in 1:ncol(ALLpromotorBeta)) {
+  for (i in 1:nrow(ALLpromotorBeta)) {       # Alle Gene dim(start) = 55111
+    if(sum(is.na(ALLpromotorBeta[i,c(6:10)]))>=4){
+      ALLpromotorBeta <- ALLpromotorBeta[-i,]
+    }
+  }
+}
+dim(ALLpromotorBeta) # dim(end) = 54587 (1% weg)
+
+# Insgesamt disease(7,9%) + healthy (1%) = 8,9% (5225) aller Gene verworfen 
+
+# NAs durch Mean der Zeile ersetzen (nach gesund und krank separiert)
+
+for (j in 1:nrow(ALLpromotorBeta)) {       # Kranke Daten
+     for (i in 1:5) {
+         if(is.na(ALLpromotorBeta[i,j])){
+             rowMeans(ALLpromotorBeta[i,c(1:5)], na.rm = TRUE) -> ALLpromotorBeta1[i,j]
          }
      }
  }
-> View(ALLBetaGen)
+for (j in 1:nrow(ALLpromotorBeta)) {       # Gesunde Daten
+  for (i in 6:10) {
+    if(is.na(ALLpromotorBeta[i,j])){
+      rowMeans(ALLpromotorBeta[i,c(6:10)], na.rm = TRUE) -> ALLpromotorBeta1[i,j]
+    }
+  }
+}
 
 # Normalisierung
 ALLMGen <- ALLBetaGen                 # Nur relevante Gene
